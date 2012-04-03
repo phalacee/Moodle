a8bda3de4fbd3a910e99f196d53a02f918768fc3
Brian Jorgensen <brian.jorgensen@ualberta.ca>
2012-04-03 11:45:43 -0600

    MDL-32180 calendar - added developer hint for a missing or unreadable calendar hook file

--- calendar/lib.php
+++ calendar/lib.php
@@ -2280,11 +2280,15 @@ class calendar_event {
         global $CFG;
         static $extcalendarinc;
         if ($extcalendarinc === null) {
-            if (!empty($CFG->calendar) && file_exists($CFG->dirroot .'/calendar/'. $CFG->calendar .'/lib.php')) {
-                include_once($CFG->dirroot .'/calendar/'. $CFG->calendar .'/lib.php');
-                $extcalendarinc = true;
-            } else {
-                $extcalendarinc = false;
+            if (!empty($CFG->calendar)) {
+                if (is_readable($CFG->dirroot .'/calendar/'. $CFG->calendar .'/lib.php')) {
+                    include_once($CFG->dirroot .'/calendar/'. $CFG->calendar .'/lib.php');
+                    $extcalendarinc = true;
+                } else {
+                    debugging("Calendar lib file missing or not readable at /calendar/{$CFG->calendar}/lib.php.",
+                        DEBUG_DEVELOPER);
+                    $extcalendarinc = false;
+                }
             }
         }
         if($extcalendarinc === false) {
